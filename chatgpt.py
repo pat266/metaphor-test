cad6dfc680188c70740870e80c8074c418b612d7
pat266 <69089199+pat266@users.noreply.github.com>
2023-10-01 18:06:22 -0400

    create vacation plan using ChatGPT API

--- chatgpt.py
+++ chatgpt.py
@@ -3,5 +3,76 @@ from dotenv import load_dotenv
 import os
 import requests
 from dotenv import load_dotenv
+import nltk
 
 load_dotenv()
+
+# Define the OpenAI API endpoint
+url = "https://api.openai.com/v1/chat/completions"
+# Get the API key from the environment variable
+api_key = os.getenv('OPENAI_API_KEY')
+# Set up the headers, including the API key
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+def chat(prompt):
+    # Set up the data to send with the POST request
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+    }
+
+    # Send the POST request to the OpenAI API
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        # Parse the JSON response
+        response_json = response.json()
+
+        # Extract the answer from the JSON response
+        answer = response_json['choices'][0]['message']['content']
+        return answer
+    else:
+        return response.text
+
+def summarize_destination(destination, contents):
+    prompt = f"Summarize the attractions with important information such as the address, hours, ticket price, \
+        what is good and popular there with the maximum of 300 words. \
+        Determine the name of the attraction here: {destination}, and this is the content: {contents}"
+    
+    # Tokenize the string.
+    tokens = nltk.word_tokenize(prompt)
+
+    # Check the length of the tokenized string.
+    if len(tokens) > 4000:
+        # Truncate the string to 4000 tokens.
+        tokens = tokens[:4000]
+
+    # Join the truncated string back into a single string.
+    truncated_prompt = " ".join(tokens)
+    return chat(truncated_prompt)
+
+
+def create_plan(destination, summarized_destinations, num_days):
+    destionation_details = ".".join(summarized_destinations)
+    prompt = f"Help me plan a vacation to {destination} for {num_days} with the following attractions and their description. \
+                For each of the attraction, only write one or two sentences about what is special about them. \
+                Here is the summarized target attraction details: {destionation_details}"
+    
+    # Tokenize the string.
+    tokens = nltk.word_tokenize(prompt)
+
+    # Check the length of the tokenized string.
+    if len(tokens) > 4000:
+        # Truncate the string to 4000 tokens.
+        tokens = tokens[:4000]
+
+    # Join the truncated string back into a single string.
+    truncated_prompt = " ".join(tokens)
+    return chat(truncated_prompt)
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import re
 import os
 from dotenv import load_dotenv
+from chatgpt import summarize_destination, create_plan
 
 load_dotenv()
 
@@ -24,9 +25,11 @@ def main():
     
     # 1. Create a dictionary to store the contents of each destination
     destination_data = {}
+    summarized_destination_list = []
 
     # 2. Accept the user input of where to go
     destination = input("Enter your destination: ")
+    num_days = input("Enter number of days: ")
 
     # 3. Search for attractions near the user input
     print(f"Searching for popular attractions near {destination}...")
@@ -48,16 +51,23 @@ def main():
         contents_response = metaphor_api.get_contents(ids)
 
         # Combine all the content extracts into a single string
-        combined_contents = " ".join([convert_to_ascii(remove_multiple_newlines(strip_html_tags(content.extract))) for content in contents_response.contents])
+        combined_contents = " ".join([convert_to_ascii(strip_html_tags(content.extract)) for content in contents_response.contents])
+        combined_contents = remove_multiple_newlines(combined_contents)
 
         # Add the combined contents to the dictionary using the result's title as the key
         destination_data[result.title] = combined_contents
 
-    # Displaying the content of each attraction
+    # 5. Summarize the contents of each destination
     for title, contents in destination_data.items():
-        print("--------------------------------------------------")
-        print(f"Title: {title}")
-        print(contents)
+        summarized_destination_list.append(summarize_destination(title, contents))
+
+    # 6 Create a plan for the user
+    generated_plan = create_plan(destination=destination, summarized_destinations=summarized_destination_list, num_days=num_days)
+    plan = f"Here is your plan to go to {destination} for {num_days} days: {generated_plan}"
+    print(plan)
+    with open("generated_plan.txt", "w") as text_file:
+        text_file.write(plan)
+        
 
 if __name__ == "__main__":
     main()
